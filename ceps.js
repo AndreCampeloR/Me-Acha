d6e08669d58a5adeb083235148af3fd94624a24d
AndreCampeloR <decocampeloreis@gmail.com>
2022-12-06 11:56:01 -0300

    Correção de bug

--- ceps.js
+++ ceps.js
@@ -6,13 +6,14 @@ window.onload = function(){
 	const dadosCep = async function(cepUrl){
 
 	try{
+
 		var url=`https://viacep.com.br/ws/${cepUrl}/json/`;
 		var consultaCep = await fetch(url);
 		var dadoCep = await consultaCep.json();
 
-			for(var campelo in dadoCep){
-				if(document.querySelector("#"+campelo)){
-					document.querySelector("#"+campelo).value = dadoCep[campelo];
+			for(var campos in dadoCep){
+				if(document.querySelector("#"+campos)){
+					document.querySelector("#"+campos).value = dadoCep[campos];
 					
 					if((document.querySelector("#logradouro").value)==""){
 						document.querySelector("#logradouro").value="Rua não encontrada"
@@ -21,24 +22,35 @@ window.onload = function(){
 						document.querySelector("#bairro").value="Bairro não encontrado"
 					}
 					if((document.querySelector("#localidade").value)==""){
-					document.querySelector("#localidade").value="Cidade não encontrada"
-				}
-				if((document.querySelector("#uf").value)==""){
-					document.querySelector("#uf").value="UF não encontrada"
-				}
-			}
-		}
+						document.querySelector("#localidade").value="Cidade não encontrada"
+				    }
+				    if((document.querySelector("#uf").value)==""){
+						document.querySelector("#uf").value="UF não encontrada"
+				    }
+					if(dadoCep[campos] === true){
+						document.querySelector("#logradouro").value = ""
+						document.querySelector("#bairro").value = ""
+						document.querySelector("#localidade").value = ""
+						document.querySelector("#uf").value = ""
+						alert("Esse Cep não existe");
+					}
+			    }
+
+		    }
+
 	}
 					
 	 catch{
-		alert("Esse Cep não existe");
+		alert("Esse não é um Cep válido");
 		
 	}
+
 }
 
 	btnBuscaCep.addEventListener("click",()=>{
 		
 		let cep = cxCep.value;
 		dadosCep(cep);
+
 	})
 }
